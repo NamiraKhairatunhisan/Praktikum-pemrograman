2b1d9a3b321f9b808076b95fd36c579f81ba7f64
NamiraKhairatunhisan <namirakhairatunhisan@mail.ugm.ac.id>
2025-09-17 06:56:47 +0700

    Update Aktivitas2b_BAB2.cpp

--- prak. pemrograman NAMIRA KHAIRATUNHISAN ELD/Aktivitas2b_BAB2.cpp
+++ prak. pemrograman NAMIRA KHAIRATUNHISAN ELD/Aktivitas2b_BAB2.cpp
@@ -2,18 +2,11 @@
 using namespace std;
 
 int main(){
-	int bil1, bil2;
-	cout<<"masukkan dua bilangan (pisahkan dengan spasi): ";
-	cin>> bil1 >> bil2;
-	
-	cout<<"bilangan pertama: "<< bil1 <<endl;
-	cout<<"bilangan kedua: "<< bil2 << endl;
-	
 	string kalimat;
-	cin.ignore();
 	cout << "masukkan sebuah kalimat: ";
 	getline(cin, kalimat);
 	cout<< "kalimat yang anda masukkan: " << kalimat<<endl;
 	
 	return 0;
 }
+
