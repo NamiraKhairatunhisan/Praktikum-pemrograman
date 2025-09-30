d54a038137ccc483b199599c0ea1fd104ddd535f
NamiraKhairatunhisan <namirakhairatunhisan@mail.ugm.ac.id>
2025-10-01 03:21:33 +0700

    Update PPHomeworkC.cpp

--- BAB4/PPHomeworkC.cpp
+++ BAB4/PPHomeworkC.cpp
@@ -3,18 +3,28 @@ using namespace std;
 
 int main(){
 	int x, fungsi, y1sebelum, y2sebelum;
-	x=0;
-	y1sebelum=-1;
-	y2sebelum=1;
+	x=3;
 	
-	for(int i=0; i<7; i++){
+	for(int i=1; i<7; i++){
+	    if(i==1 || i==2){
+	        cout << "untuk x= " << i<<endl;
+	        fungsi = 1;
+	        cout << "y= " << fungsi<<endl<<endl;
+	        
+	        y1sebelum = 1;
+	        y2sebelum = 1;
+	        
+	    }
+	    
+	    else{
 		fungsi = y1sebelum + y2sebelum;
 		
 		cout << "untuk x= " << x<<endl;
-		cout<< "y= " << -1*fungsi<<endl<<endl;
+		cout<< "y= " << fungsi<<endl<<endl;
 		x+= 1;
 		y2sebelum=y1sebelum;
 		y1sebelum=fungsi;
+	    }
 	}
 	
 }
