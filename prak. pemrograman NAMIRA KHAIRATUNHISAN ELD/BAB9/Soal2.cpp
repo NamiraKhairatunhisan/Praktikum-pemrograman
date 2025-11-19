f2e2425fbc4fc769ecbb03dc8de3be9b612a4d27
NamiraKhairatunhisan <namirakhairatunhisan@mail.ugm.ac.id>
2025-11-19 16:24:25 +0700

    Update Soal2.cpp

--- prak. pemrograman NAMIRA KHAIRATUNHISAN ELD/BAB9/Soal2.cpp
+++ prak. pemrograman NAMIRA KHAIRATUNHISAN ELD/BAB9/Soal2.cpp
@@ -18,7 +18,7 @@ void bubble_sort(int data[], int n){
 	
 	for (int i=0; i<n; i++){
 		if(i==n-1)
-		cout << data[i]<< " ";
+		cout << data[i]<< "";
 		else
 		cout << data[i]<< ", ";
 	}
@@ -39,19 +39,32 @@ int main (){
 	int ganjil[1000], genap[1000], nGan, nGen; 
 	nGen=0;
 	nGan=0;
+	bool adaGan = false;
+	bool adaGen = false;
 	
 	for (int i=0; i<n; i++){
 		if (data[i]%2==1){
 			nGan++;
 			ganjil[nGan-1] = data[i];
+			adaGan = true;
 		}
 		if(data[i]%2==0){
 			nGen++;
 			genap[nGen-1] = data[i];
+			adaGen = true;
 		}
 	}
-	
-	bubble_sort(ganjil, nGan);
-	bubble_sort(genap, nGen);
+
+	if (adaGan && adaGen){
+		bubble_sort(ganjil, nGan);
+		cout << ", ";
+		bubble_sort(genap, nGen);
+		
+	}
+	else{
+		bubble_sort(ganjil, nGan);
+		bubble_sort(genap, nGen);
+	}
 	return 0;
 }
+
