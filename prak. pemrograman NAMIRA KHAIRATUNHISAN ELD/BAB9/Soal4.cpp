bc2bdfb844eb2561032241b3a2865252dcb38fb1
NamiraKhairatunhisan <namirakhairatunhisan@mail.ugm.ac.id>
2025-11-19 17:25:37 +0700

    Update Soal4.cpp

--- prak. pemrograman NAMIRA KHAIRATUNHISAN ELD/BAB9/Soal4.cpp
+++ prak. pemrograman NAMIRA KHAIRATUNHISAN ELD/BAB9/Soal4.cpp
@@ -1,47 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int urut[1000];
-
 void swap(int &a, int &b) {
-int temp;
-temp = a;
-a = b;
-b = temp;
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 void bubble_sort(int data[], int n){
-	for (int i =0; i<n-1; i++){
-		for (int j=n-1; j>i; j--){
-			if (data[j] < data[j-1])
-			swap(data[j-1], data[j]);
-		}
-	}
+    for (int i = 0; i < n - 1; i++){
+        for (int j = n - 1; j > i; j--){
+            if (data[j] < data[j - 1])
+                swap(data[j], data[j - 1]);
+        }
+    }
 }
 
 int main (){
-	int n;
-	
-	cout<<"jumlah data: ";
-	cin >> n;
-	int data[n];
-	
-	cout<< "masukkan data: "<< endl;
-	for(int i=0; i<n; i++){
-		cin>> data[i];
-	}
-	
-	bubble_sort(data, n);
-	float median;
-	if (n%2==0){
-		median = n/2;
-		cout<< "median: " << data[median-1];
-	}
-	if (n%2!=0){
-		median = data[(n+1)/2-1]+data[(n+1)/2];
-		cout<< "median: " << data[median-1];
-	}
-	
-	return 0;
+    int n;
+    cin >> n;
+    int data[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> data[i];
+    }
+    bubble_sort(data, n);
+
+    double median;
+
+    if (n % 2 != 0) {  
+        // ganjil → ambil elemen tengah
+        median = data[n/2];
+        cout << median;
+    }
+    else {            
+        // genap → rata-rata dua elemen tengah
+        median = (data[n/2 - 1] + data[n/2]) / 2.0;
+        cout << median;
+    }
+
+    return 0;
 }
+
 
